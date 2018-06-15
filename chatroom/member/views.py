0bbb9803e9dd40d28abe9e3c16695e7983e6bd89
darkblank <darkblank1990@gmail.com>
2018-06-15 18:52:38 +0900

    signup 페이지 생성

--- chatroom/member/views.py
+++ chatroom/member/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.contrib.auth import login as django_login
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from member.forms import UserSignUpForm
+
+
+def signup(request):
+    if request.method == 'POST':
+        user_form = UserSignUpForm(request.POST, request.FILES)
+        if user_form.is_valid():
+            user = user_form.save()
+            django_login(request, user)
+            return redirect('chat:index')
+    else:
+        user_form = UserSignUpForm()
+    context = {
+        'user_form': user_form
+    }
+    return render(request, 'member/signup.html', context)
