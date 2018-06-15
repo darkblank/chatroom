95d5b36c792948faad0b132b32c44a6093830dc0
darkblank <darkblank1990@gmail.com>
2018-06-15 22:30:24 +0900

    minor fix

--- chatroom/member/views.py
+++ chatroom/member/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import login as django_login, logout
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
